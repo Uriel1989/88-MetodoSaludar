8c6160cab67376764409cc57047019f431868298
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:56:04 -0300

    Add files via upload

---
+++ 88-MetodoSaludar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//Crea un método que nos salude, pasándole un nombre por parámetro.
+
+namespace _88_MetodoSaludar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Saludar saludar = new Saludar("Uriel");
+        }
+    }
+}
---
+++ 88-MetodoSaludar/Saludar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _88_MetodoSaludar
+{
+    class Saludar
+    {
+        public String nombre;
+
+        public Saludar( string nom)
+        {
+            Console.WriteLine("Hola, sientete bienvenido " + (this.nombre = nom));
+        }
+
+        
+    }
+}
